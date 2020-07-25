a2a3b896522f19eb7eacaea6d421bf3cec881b8f
Dmitry Ivanov-Home <securiti.zp.ua>
2020-07-25 21:02:51 +0300

    readonly migration2

---
+++ server/src/db/config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "development": {
+        "username": 'ids',
+        "password": 'a3q2f5',
+        "database": 'todo_db',
+        "host": '127.0.0.1',
+        "dialect": 'postgres'
+    },
+    "test": {},
+    "production": {}
+};
--- server/src/db/models/task.js
+++ server/src/db/models/task.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class task extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,13 +13,13 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   };
-  User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
+  task.init({
+    value: DataTypes.STRING,
+    isDone: DataTypes.BOOLEAN,
+    deadline: DataTypes.DATE
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'task',
   });
-  return User;
+  return task;
 };
